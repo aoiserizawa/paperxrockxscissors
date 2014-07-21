fc476f656851108c76dea728e7ac7c12993800f8
alvin <alvinjosephvaldez@yahoo.com>
2014-07-21 23:38:01 +0800

    todo: outputing the available users

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -9,10 +9,16 @@ $(document).ready(function(){
 		e.preventDefault();
 		socket.emit('newUser', $userName.val(), function(data){
 			if(data){
-                     
-             }else{
-                       
+                   
+             }else{ 
+               console.log("already taken");    
              }
+		});	
+	});
+
+	socket.on('showUsers', function(data){
+		$.each(data, function(key, value){
+			console.log(key+" : "+value);
 		});
 	});
 
--- routes/socket.js
+++ routes/socket.js
@@ -6,15 +6,28 @@ exports.initialize = function(server){
 
     io.sockets.on('connection', function(socket){
        socket.on('newUser', function(data, callback){
+
 	       	if(data in users){	
 	       		callback(false);
+
+                // NOTE: probably not needed
+                // check if socket is already in the users objects
+                // for (var user in users){
+                //     var ids = users[user];
+                //     for (var id in ids ){
+                //         if(ids[id] === socket.id){
+                //             console.log("in use :"+socket.id);
+                //         }
+                //     } 
+                // }
+
 	       	}else{
 	       		callback(true);
 	       		socket.username = data;
 	       		users[socket.username] = {'id' : socket.id};
+                io.sockets.emit('showUsers', users);
 	       	}
 
-	       	console.log(users);
        });
     });
 };
